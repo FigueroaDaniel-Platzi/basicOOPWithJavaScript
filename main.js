adb46f1a737c653caeb79f7af13790d4d490d101
Daniel Figueroa <da.figuepa@gmail.com>
2023-01-09 18:15:26 -0500

    Clases en JavaScript class6

--- main.js
+++ main.js
@@ -21,3 +21,23 @@ Student.prototype.aprobarCurso = function(nuevoCursito) {
 }
 
 const juanita = new Student('Juanita Alejandra', 15, ['Curso de Introducción a la Producción de Videojuegos', 'Curso de Creación de Personajes'])
+
+// Prototipos con la sintaxis de clases
+class Student2 {
+  constructor({
+    name,
+    cursosAprobados = [],
+    age,
+    email
+  }) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados; 
+  }
+
+  aprobarCurso(nuevoCursito) {
+    this.cursosAprobados.push(nuevoCursito);
+  }
+};
+
+const miguelito = new Student2({name: "Miguel", age: 28, email: "miguelito@platzi.com"});
